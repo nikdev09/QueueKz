e776fd7dfaaa21ec09b1a21bd7089a73002c5a9a
NikOwner <105271863+NikOwner@users.noreply.github.com>
2022-05-10 17:04:56 +0600

    Create insetvalues.php

---
+++ insetvalues.php
@@ -0,0 +1,6 @@
+<center><form name="form1" method="post" action="/insert.php">
+ Введите имя:<br />
+ <textarea name="name" cols="80" rows="10"></textarea>
+ <input name="" type="submit" value="Отправить"/>
+ </form></center>
+ <?php echo date('Y-m-d H:i:s'); ?>
